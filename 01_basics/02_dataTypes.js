328b5feaaeb5e18a5de97eefbdfb33ada380fc60
Yash Khare <47681673+Ekluvya@users.noreply.github.com>
2026-04-05 08:33:50 +0000

    Learned about Javascript Datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+//alert("Hello") //we are using nodejs not browser
+
+console.log(3+3) //code readability matters and should be high
+
+let name = "Yash"
+let age = 18
+let isLoggedIn = false
+
+//number ==> 2 to power 53
+//bigint
+//string => ""
+//boolean ==> true/false
+//null ==> standalone value
+//undefined ==> 
+//symbol ==> unique
+
+//object
+
+console.log(typeof null)
+console.log(typeof undefined)
