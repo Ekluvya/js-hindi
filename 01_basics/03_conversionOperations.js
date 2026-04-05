9769c29aef844a52168030ebde8ee9ca092d15e0
Yash Khare <47681673+Ekluvya@users.noreply.github.com>
2026-04-05 08:53:21 +0000

    Learned about data type conversion in Javascript

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,30 @@
+let age = "35"
+
+console.log(typeof age)
+
+let valueInNumber = Number(age)
+
+console.log(typeof valueInNumber)
+
+let NAN = "45ab"
+let NAN_num = Number(NAN)
+console.log(typeof NAN_num)
+console.log(NAN_num)
+
+//"33" ==> 33
+//"33abc" ==> NaN
+//true ==>1; false => 0
+
+let isLoggedIn = 1
+let booleansLoggedIn = Boolean(isLoggedIn)
+console.log(booleansLoggedIn)
+
+// 1==>true
+// 0==>false
+// "" ==> false
+//"yash" ==> true
+
+let number = 33
+let stringNumber = String(number)
+console.log(typeof stringNumber)
+console.log(number)
